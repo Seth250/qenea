21a9477a6ad8e19477b48e7afeb55a118442ef65
Seth250 <ayowaleakintayo@gmail.com>
2020-10-06 22:39:50 +0100

    not so initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+	<header>
+		<h1><a href="/">Qenea</a></h1>
+	</header>
+</template>
+
+<script>
+import NavBar from './NavBar'
+
+export default {
+	name: 'Header'
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+	<nav>
+		<ul v-for="item in navItems">
+			<li>
+				<router-link :to="#">{{ item }}</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'NavBar',
+	props: {
+		navItems: Array
+	}
+}
+</script>
