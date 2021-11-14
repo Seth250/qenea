48148037a4ec2c08009dc4630e3d8cbd8a168d16
seth250 <ayowaleakintayo@gmail.com>
2021-11-14 22:28:50 +0100

    update nav-bar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,29 @@
 <template>
   <nav class="nav-bar">
-    <ul class="nav-bar__items">
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link to="/">Blog</router-link>
-      </li>
-    </ul>
-    <div>
-    </div>
+    <template v-if="true">
+      <ul class="nav-bar__items">
+        <li>
+          <router-link class="nav-bar__link" to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link class="nav-bar__link" to="/">Blog</router-link>
+        </li>
+      </ul>
+      <div class="nav-bar__user">
+        <div>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <ul class="nav-bar__items">
+        <li>
+          <router-link class="nav-bar__link" to="/">Login</router-link>
+        </li>
+        <li>
+          <router-link class="nav-bar__link" to="/">Signup</router-link>
+        </li>
+      </ul>
+    </template>
   </nav>
 </template>
 
