3789f0638479b5c74a75ea5ac9a97a801562e691
seth250 <ayowaleakintayo@gmail.com>
2021-11-14 12:29:12 +0100

    configure vue-router and vuex

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/scss/index.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.use(store)
+
+app.mount('#app')
