9b47e5976d9df3ae1e39ffe9485deb41d4b4e02f
seth250 <ayowaleakintayo@gmail.com>
2021-11-14 12:35:00 +0100

    add nav-bar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="nav-bar">
+    <ul class="nav-bar__items">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/">Blog</router-link>
+      </li>
+    </ul>
+    <div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
