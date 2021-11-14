1cca05b75f289b6c195fc0e4f0d8f3a54dd61fd6
seth250 <ayowaleakintayo@gmail.com>
2021-11-14 22:28:18 +0100

    add page header component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <h1>
+      <a href="/">Qenea</a>
+    </h1>
+    <NavBar />
+  </header>
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+
+export default {
+  name: 'PageHeader',
+  components: {
+    NavBar
+  }
+}
+</script>
