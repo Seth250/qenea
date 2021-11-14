b390de52cfe0913b5bd5b887ff768763cc91938e
seth250 <ayowaleakintayo@gmail.com>
2021-11-14 22:38:45 +0100

    add login and signup routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: () => import(/* webpackChunkName: "signup" */ '@/views/SignUp.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
