0274fbfa219e95d88b2ce9ebb9d9cb669977a086
seth250 <ayowaleakintayo@gmail.com>
2021-11-15 19:24:11 +0100

    add login and signup navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,43 @@
 import { createWebHistory, createRouter } from 'vue-router'
+import store from '../store'
 
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+  },
+  {
     path: '/login',
     name: 'Login',
-    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue'),
+    meta: {
+      requiresGuest: true
+    }
   },
   {
     path: '/signup',
     name: 'SignUp',
-    component: () => import(/* webpackChunkName: "signup" */ '@/views/SignUp.vue')
+    component: () => import(/* webpackChunkName: "signup" */ '@/views/SignUp.vue'),
+    meta: {
+      requiresGuest: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresGuest)) {
+    if (store.getters.loggedIn) {
+      next({ name: 'Home' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
