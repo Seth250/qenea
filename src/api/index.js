8b8987ab39c1fadecda571a9c24bd42aa7804e13
seth250 <ayowaleakintayo@gmail.com>
2021-11-16 10:27:29 +0100

    add loader to api requests

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import app from '@/main.js' // mounted AFTER this file is loaded (will be undefined if used immediately and not in a callable)
 
 const baseURL = process.env.VUE_APP_API
 
@@ -8,5 +9,34 @@ const instance = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+// loader instance
+let loader = null
+
+const hideLoader = () => {
+  loader && loader.hide()
+  loader = null
+}
+
+instance.interceptors.request.use(
+  config => {
+    loader = app.config.globalProperties.$loading.show()
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+instance.interceptors.response.use(
+  response => {
+    hideLoader()
+    return response
+  },
+  error => {
+    hideLoader()
+    Promise.reject(error)
+  }
+)
 
 export default instance
