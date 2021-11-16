d8eb0e7cf7223cb04685a7e4dc6581c2dd37a22a
seth250 <ayowaleakintayo@gmail.com>
2021-11-16 10:07:22 +0100

    add vue loader plugin to app

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLoading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
 import './assets/scss/index.scss'
 
 const app = createApp(App)
 
-app.use(router)
-
 app.use(store)
 
+app.use(router)
+
+app.use(VueLoading, {
+  color: '#222'
+})
+
 app.mount('#app')
+
+export default app
