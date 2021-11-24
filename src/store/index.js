1cdc1432d216b9a242e5e7334ddff84cf2d47d8d
seth250 <ayowaleakintayo@gmail.com>
2021-11-24 09:25:09 +0100

    add questions and answers store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import auth from './modules/auth.js'
+import questans from './modules/questans.js'
 
 const store = createStore({
   modules: {
-    auth
+    auth,
+    questans
   }
 })
 
---
+++ src/store/modules/questans.js
@@ -0,0 +1,35 @@
+import axiosBase from '@/api'
+
+const state = {
+  questions: [],
+  totalQuestions: 0
+}
+
+const getters = {
+  pageQuestions: state => state.questions
+}
+
+const actions = {
+  async fetchPageQuestions ({ commit }, pageNumber) {
+    const params = {}
+    if (pageNumber > 1) {
+      params.page = pageNumber
+    }
+    const { data } = await axiosBase.get('questions', { params })
+    commit('setPageQuestions', data)
+  }
+}
+
+const mutations = {
+  setPageQuestions (state, { results, count }) {
+    state.questions = results
+    state.totalQuestions = count
+  }
+}
+
+export default {
+  state: () => state,
+  actions,
+  getters,
+  mutations
+}
