bc0cf1c00d6992b4bc7d7fcd9c4371e6296fba1a
seth250 <ayowaleakintayo@gmail.com>
2021-12-05 20:08:21 +0100

    add field error component

---
+++ src/components/FieldError.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul v-if="errors && errors.length" class="form__error-list">
+    <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'FieldError',
+  props: {
+    errors: {
+      type: Array,
+      required: false
+    }
+  }
+}
+</script>
