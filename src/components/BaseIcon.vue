3dbe275993ef9109a5153100721d853014f405e3
seth250 <ayowaleakintayo@gmail.com>
2021-12-10 22:19:34 +0100

    add icon component and directory

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="`${name}Icon`"></component>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+export default {
+  name: 'BaseIcon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  components: {
+    AddIcon: defineAsyncComponent(() => import('./__private__/media/AddIcon.vue')),
+    LeftCaretIcon: defineAsyncComponent(() => import('./__private__/media/LeftCaretIcon.vue')),
+    RightCaretIcon: defineAsyncComponent(() => import('./__private__/media/RightCaretIcon.vue'))
+  }
+}
+</script>
